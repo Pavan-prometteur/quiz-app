705c3120e5bb7d6b17b1ff4c25a59c578c2c96ab
Pavan Udavant <pavan.u@prometteur.in>
2024-07-11 12:24:13 +0530

    commit

--- addmcq.js
+++ addmcq.js
@@ -11,8 +11,9 @@ export let mcqs=[
         Q: "Which planet in the solar system is known as the “Red Planet”?",
         a:"Venus",
         b:"Earth",
-        C:"Mars",
+        c:"Mars",
         d:"Jupiter",
+        correctAnswer:"Mars"
     },
     {
         Q:'What is the capital of Japan?',
@@ -38,28 +39,4 @@ export let mcqs=[
         d:"Carbon dioxide",
         correctAnswer:"Nitrogen"
     },
-    // {
-    //     Q1:'What is the largest lake in the world?',
-    //     a:"Caspian Sea",
-    //     b:"Baikal",
-    //     c:"Lake Superior",
-    //     d:"Ontario",
-    //     correctAnswer:"Baikal"
-    // },
-    // {
-    //     Q1:'What is the largest lake in the world?',
-    //     a:"Caspian Sea",
-    //     b:"Baikal",
-    //     c:"Lake Superior",
-    //     d:"Ontario",
-    //     correctAnswer:"Baikal"
-    // },
-    // {
-    //     Q1:'What is the largest lake in the world?',
-    //     a:"Caspian Sea",
-    //     b:"Baikal",
-    //     c:"Lake Superior",
-    //     d:"Ontario",
-    //     correctAnswer:"Baikal"
-    // }
 ]
--- quiz.js
+++ quiz.js
@@ -1,54 +1,46 @@
 import { mcqs } from "./addmcq.js";
 
-let mcqListHTML = "";
 let score = 0;
+let currentQuestion = 0;
 
-mcqs.forEach(function (q1, index) {
-    const { Q, a, b, c, d } = q1;
-    let questions = `<div class="mcqList">
-    <label>${Q}</label><br><br>
-    <input type="radio" name="ans${index}" value="${a}">
-    <label>${a}</label><br><br>
-    <input type="radio" name="ans${index}" value="${b}">
-    <label>${b}</label><br><br>
-    <input type="radio" name="ans${index}" value="${c}">
-    <label>${c}</label><br><br>
-    <input type="radio" name="ans${index}" value="${d}">
-    <label>${d}</label><br></br>
-    </div>`;
+function showQuestion() {
+    const { Q, a, b, c, d } = mcqs[currentQuestion];
+    const questions = `
+        <div class="mcqList">
+            <label>Q.${Q}</label><br><br>
+            <input type="radio" name="ans${currentQuestion}" value=${a}>
+            <label>${a}</label><br><br>
+            <input type="radio" name="ans${currentQuestion}" value=${b}>
+            <label>${b}</label><br><br>
+            <input type="radio" name="ans${currentQuestion}" value=${c}>
+            <label>${c}</label><br><br>
+            <input type="radio" name="ans${currentQuestion}" value=${d}>
+            <label>${d}</label><br><br>
+        </div>`;
 
-    mcqListHTML += questions;
-});
+    const page = document.querySelector('.mcq');
+    page.innerHTML = questions;
+    document.querySelector(".page-no").innerHTML=`<p>${currentQuestion+1}/${mcqs.length}</p>`;
 
-let page = document.querySelector('.mcq');
-page.innerHTML = mcqListHTML + `<br><button type="button" onclick=${getAllAnswers()}>Submit</button>`;
-
-function getAllAnswers() {
-    let eachMcq = document.getElementsByClassName('mcqList');
-    
-    for (let i = 0; i < eachMcq.length; i++) {
-        let radioButtons = eachMcq[i].querySelectorAll('input[type="radio"]');
-        console.log(radioButtons);
-        let selectedValue = null;
-
-        for (let j = 0; j < radioButtons.length; j++) {
-            if (radioButtons[j].checked) {
-                selectedValue = radioButtons[j].value;
-                break;
-            }
-        }
-
-        let correctAnswer = mcqs[i].correctAnswer;
-
-        if (selectedValue !== null) {
-            console.log('Question ' + (i + 1) + ': Selected Value = ' + selectedValue);
-            if (selectedValue === correctAnswer) {
-                score++;
-            }
-        } else {
-            console.log('Question ' + (i + 1) + ': No option selected');
-        }
-    }
-    
-    console.log('Your score is: ' + score + '/' + mcqs.length);
+    const radioButtons = page.querySelectorAll(`input[name="ans${currentQuestion}"]`);
+    radioButtons.forEach(radio => {
+        radio.addEventListener('click', () => checkAnswer(radio.value));
+    });
 }
+
+function checkAnswer(selected) {
+    const correctAnswer = mcqs[currentQuestion].correctAnswer;
+    if (selected === correctAnswer) {
+        score++;
+    }
+    currentQuestion++;
+
+    if (currentQuestion < mcqs.length) {
+        showQuestion();
+    } else {
+        const quizContainer = document.querySelector(".quiz-container");
+        quizContainer.innerHTML = `<p style="font-size: 30px; color: blue; font-weight: bold; text-align: center; margin-top: 50px">You got ${score} out of ${mcqs.length} questions.</p>`;
+    }
+}
+
+showQuestion();
